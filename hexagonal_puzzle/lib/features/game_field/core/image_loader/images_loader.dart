465c2f7fce665fd8612733a02d5f6ebddde7fdc8
Alexander Shevelev <al.e.shevelev@gmail.com>
2026-01-09 22:55:19 +0300

    The app has been moved to Image library instead of Bitmap

--- hexagonal_puzzle/lib/features/game_field/core/image_loader/images_loader.dart
+++ hexagonal_puzzle/lib/features/game_field/core/image_loader/images_loader.dart
@@ -2,8 +2,7 @@ import 'dart:async';
 import 'dart:ui' as ui;
 import 'dart:math' as math;
 
-import 'package:bitmap/bitmap.dart';
-import 'package:flutter/foundation.dart';
+import 'package:image/image.dart' as img;
 import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 
@@ -11,19 +10,18 @@ import '../game_field_calculator/dto/game_field_points_dto.dart';
 import '../game_field_calculator/dto/hexagon_points_dto.dart';
 import '../game_field_calculator/dto/piece_points_dto.dart';
 import '../game_field_calculator/game_field_calculator.dart';
-import 'bitmap_utils.dart';
 import 'dto/game_field_fixed_piece.dart';
 import 'dto/game_field_hex.dart';
 import 'dto/game_field_model.dart';
 
 class ImageLoader {
   Future<GameFieldModel> loadImages(String asset, Size canvasSize, int piecesInRow) async {
-    final assetBitmap = await _loadAssetBitmap(asset);
+    final assetImage = await _loadAssetImage(asset);
 
     final gameFieldCalculator = GameFieldCalculator();
     final calculationResult = gameFieldCalculator.calculatePieces(canvasSize, piecesInRow);
 
-    final resizedBitmap = await _resizeBitmap(assetBitmap, calculationResult.gameFieldSize);
+    final resizedBitmap = await _resizeImage(assetImage!, calculationResult.gameFieldSize);
 
     final gameFieldOffset = Offset(
       (canvasSize.width - calculationResult.gameFieldSize.width) / 2,
@@ -32,60 +30,38 @@ class ImageLoader {
     return await _cutAllPieces(resizedBitmap, calculationResult, gameFieldOffset);
   }
 
-  Future<Bitmap> _loadAssetBitmap(String asset) async {
+  Future<img.Image?> _loadAssetImage(String asset) async {
     final ByteData data = await rootBundle.load(asset);
     final imageDataBuffer = Uint8List.view(data.buffer);
-    return await Bitmap.fromProvider(MemoryImage(imageDataBuffer));
+    return img.decodeWebP(imageDataBuffer);
   }
 
-  Future<Bitmap> _resizeBitmap(Bitmap source, Size targetSize) async {
+  Future<img.Image> _resizeImage(img.Image source, Size targetSize) async {
     final sizeFactor = targetSize.height / source.height;
     final newSourceSize = Size(source.width * sizeFactor, targetSize.height);
 
-    if (newSourceSize.width <= targetSize.width) {
-      final resized = await compute(
-        resizeBitmap,
-        [
-          source.content,
-          source.width,
-          source.height,
-          targetSize.width.toInt(),
-          targetSize.height.toInt(),
-        ],
+    final command = img.Command()
+      ..image(source)
+      ..copyResize(
+        width: newSourceSize.width.toInt(),
+        height: newSourceSize.height.toInt(),
+        interpolation: img.Interpolation.cubic,
       );
 
-      return Bitmap.fromHeadless(targetSize.width.toInt(), targetSize.height.toInt(), resized);
-    } else {
-      final resized = await compute(
-        resizeBitmap,
-        [
-          source.content,
-          source.width,
-          source.height,
-          newSourceSize.width.toInt(),
-          newSourceSize.height.toInt(),
-        ],
+    if (newSourceSize.width > targetSize.width) {
+      command.copyCrop(
+        x: (newSourceSize.width - targetSize.width) ~/ 2,
+        y: 0,
+        width: targetSize.width.toInt(),
+        height: targetSize.height.toInt(),
       );
-
-      final cropped = await compute(
-        cropBitmap,
-        [
-          resized,
-          newSourceSize.width.toInt(),
-          newSourceSize.height.toInt(),
-          (newSourceSize.width - targetSize.width) ~/ 2,
-          0,
-          targetSize.width.toInt(),
-          targetSize.height.toInt(),
-        ],
-      );
-
-      return Bitmap.fromHeadless(targetSize.width.toInt(), targetSize.height.toInt(), cropped);
     }
+
+    return (await command.executeThread()).outputImage!;
   }
 
   Future<GameFieldModel> _cutAllPieces(
-    Bitmap source,
+    img.Image source,
     GameFieldPointsDto points,
     Offset gameFieldOffset,
   ) async {
@@ -115,12 +91,12 @@ class ImageLoader {
 
     final hexes = await _cutHex(source, points.hexagons);
 
-    final ui.Image sourceImage = await source.buildImage();
+    final ui.Image sourceImage = await _imageToUiImage(source);
 
     return GameFieldModel(fixed, hexes, gameFieldOffset, sourceImage);
   }
 
-  Future<List<GameFieldFixedPiece>> _cutFixedPieces(Bitmap source, List<PiecePointsDto> points) async {
+  Future<List<GameFieldFixedPiece>> _cutFixedPieces(img.Image source, List<PiecePointsDto> points) async {
     final pieces = List<GameFieldFixedPiece>.empty(growable: true);
 
     for (var point in points) {
@@ -133,7 +109,7 @@ class ImageLoader {
     return pieces;
   }
 
-  Future<List<GameFieldHex>> _cutHex(Bitmap source, List<HexagonPointsDto> points) async {
+  Future<List<GameFieldHex>> _cutHex(img.Image source, List<HexagonPointsDto> points) async {
     final hexes = List<GameFieldHex>.empty(growable: true);
 
     for (var point in points) {
@@ -156,25 +132,12 @@ class ImageLoader {
     return hexes;
   }
 
-  Future<ui.Image> _cutFixedPiece(Bitmap source, Rect cropRect, List<Offset> relativeVertexes) async {
+  Future<ui.Image> _cutFixedPiece(img.Image source, Rect cropRect, List<Offset> relativeVertexes) async {
     // Cut a small rect
-    final converted = await compute(
-      cropBitmap,
-      [
-        source.content,
-        source.width,
-        source.height,
-        cropRect.left.toInt(),
-        cropRect.top.toInt(),
-        cropRect.width.toInt(),
-        cropRect.height.toInt(),
-      ],
-    );
+    final cropped = _cropImage(source, cropRect);
 
-    final smallBitmap = Bitmap.fromHeadless(cropRect.width.toInt(), cropRect.height.toInt(), converted);
-
-    // To image
-    final ui.Image image = await smallBitmap.buildImage();
+    // Convert to dart ui.Image
+    final image = await _imageToUiImage(cropped);
 
     // Clip path
     var pictureRecorder = ui.PictureRecorder();
@@ -187,30 +150,21 @@ class ImageLoader {
 
     var pic = pictureRecorder.endRecording();
 
-    ui.Image img = await pic.toImage(image.width, image.height);
+    ui.Image result = await pic.toImage(image.width, image.height);
 
-    return img;
+    return result;
   }
 
-  Future<Map<GameFieldHexAngle, ui.Image>> _cutHexPiece(Bitmap source, Rect cropRect, List<Offset> relativeVertexes) async {
+  Future<Map<GameFieldHexAngle, ui.Image>> _cutHexPiece(
+    img.Image source,
+    Rect cropRect,
+    List<Offset> relativeVertexes,
+  ) async {
     // Cut a small rect
-    final converted = await compute(
-      cropBitmap,
-      [
-        source.content,
-        source.width,
-        source.height,
-        cropRect.left.toInt(),
-        cropRect.top.toInt(),
-        cropRect.width.toInt(),
-        cropRect.height.toInt(),
-      ],
-    );
-
-    final smallBitmap = Bitmap.fromHeadless(cropRect.width.toInt(), cropRect.height.toInt(), converted);
+    final cropped = _cropImage(source, cropRect);
 
     // To image
-    final ui.Image image = await smallBitmap.buildImage();
+    final ui.Image image = await _imageToUiImage(cropped);
 
     final result = <GameFieldHexAngle, ui.Image>{};
 
@@ -226,7 +180,8 @@ class ImageLoader {
       final rotationAngle = _getAngleInRadians(angle);
       canvas.rotate(rotationAngle);
 
-      final clipVertex = relativeVertexes.map((e) => e.translate(-center.dx, -center.dy)).toList(growable: false);
+      final clipVertex =
+          relativeVertexes.map((e) => e.translate(-center.dx, -center.dy)).toList(growable: false);
 
       Path path = Path()..addPolygon(clipVertex, true);
 
@@ -262,4 +217,43 @@ class ImageLoader {
         return 5 / 3 * math.pi;
     }
   }
+
+  /// From here https://github.com/brendan-duncan/image/blob/main/doc/flutter.md#convert-a-dart-image-library-image-to-a-flutter-ui-image
+  Future<ui.Image> _imageToUiImage(img.Image image) async {
+    if (image.format != img.Format.uint8 || image.numChannels != 4) {
+      final cmd = img.Command()
+        ..image(image)
+        ..convert(format: img.Format.uint8, numChannels: 4);
+      final rgba8 = await cmd.getImageThread();
+      if (rgba8 != null) {
+        image = rgba8;
+      }
+    }
+
+    ui.ImmutableBuffer buffer = await
+    ui.ImmutableBuffer.fromUint8List(image.toUint8List());
+
+    ui.ImageDescriptor id = ui.ImageDescriptor.raw(
+        buffer,
+        height: image.height,
+        width: image.width,
+        pixelFormat: ui.PixelFormat.rgba8888);
+
+    ui.Codec codec = await id.instantiateCodec(
+        targetHeight: image.height,
+        targetWidth: image.width);
+
+    ui.FrameInfo fi = await codec.getNextFrame();
+    ui.Image uiImage = fi.image;
+
+    return uiImage;
+  }
+
+  img.Image _cropImage(img.Image source, Rect cropRect) => img.copyCrop(
+      source,
+      x: cropRect.left.toInt(),
+      y: cropRect.top.toInt(),
+      width: cropRect.width.toInt(),
+      height: cropRect.height.toInt(),
+    );
 }
